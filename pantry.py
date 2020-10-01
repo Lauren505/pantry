d6c0f6c3e6865d7a5c59ac193bbc03eaf829d718
Lauren505 <59271626+Lauren505@users.noreply.github.com>
2020-10-01 22:44:46 +0800

    Create pantry.py

---
+++ pantry.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+class pantry:
+    def __init__(self):
+        self.conn =  psycopg2.connect(database='pantry',
+                                      user='postgres',
+                                      password='laupostgre01',
+                                      host='34.80.136.214',
+                                      port='5432')
+        self.cur = self.conn.cursor()
+        self.item = ""
+        self.weight = 0
+        print("Database connected!")
+
+new = pantry()
+new.cur.execute("INSERT INTO recipe (item, category, weight) VALUES ('apple', 'fruit', 20)")
+new.conn.commit()
+    
