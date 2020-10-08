361cb09be21f6e7c5411914ee4b0c0bc68aee880
zhuangggg <zhuangyichen.0916@gmail.com>
2020-10-08 18:26:37 +0800

    Create serial.py

---
+++ serial.py
@@ -0,0 +1,24 @@
+mport serial, sys
+ 
+ser = serial.Serial('/dev/ttyS0', 9600, timeout=5)
+ser.flushInput()
+ser.flushOutput()
+ 
+ 
+if not ser.isOpen():
+    ser.open()
+    print("Start receive...")
+ 
+ 
+try:
+    while True:
+        if not ser.isOpen():
+            ser.open()
+        # if ser.in_waiting:
+        data_raw = ser.readline()
+        print(data_raw.decode(),end="")
+        data = sys.stdin.readline()
+        ser.write(str.encode(data))
+except KeyboardInterrupt:
+    ser.close()
+    print('bye')
