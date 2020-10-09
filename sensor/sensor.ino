f361af9a6f25cca24d483f534ee973c5eec9b9ab
zhuangggg <64473970+zhuangggg@users.noreply.github.com>
2020-10-10 02:20:22 +0800

    Add files via upload

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,72 +1,32 @@
-#include <Wire.h>
-
-#include <HX711.h>
+#include "HX711.h"
 #include "DHT.h"
 
 #define dhtPin 8     
 #define dhtType DHT11 
-// HX711
+// HX711 接線設定
 const int DT_PIN = 6;
 const int SCK_PIN = 5;
 const int SWITCH_PIN = 2;
-
-const int scale_factor = -200;
-
+const int sample_weight = 150;  //基準物品的真實重量(公克)
 bool isOpen = 0;
 
-int k = 999;
 
-int current_data[6] = {0};
-
-HX711 scale;
+HX711 scale(5, 6, 750);
 DHT dht(dhtPin, dhtType); // Initialize DHT sensor
 
 void setup() {
   Serial.begin(9600);
-  
-  Wire.begin(0x04);
-  // Call receiveEvent when data received                
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(send_data);
-
-  //scale.begin();
-  //delay(3000);
-  //scale.begin();
-  scale.begin(DT_PIN, SCK_PIN);
-  Serial.println("Before setting up the scale:"); 
-  Serial.println(scale.get_units(5), 0);  //未設定比例參數前的數值
-  scale.set_scale(scale_factor);       // 設定比例參數
-  scale.tare();               // 歸零
-  Serial.println("After setting up the scale:"); 
-  Serial.println(scale.get_units(5), 0);  //設定比例參數後的數值
-  Serial.println("Readings:"); 
+  scale.begin();
+  delay(3000);
+  scale.begin();
+  //scale.set_scale();  // 開始取得比例參數
+  //scale.tare();
+  //Serial.println("Nothing on it.");
+  //Serial.println(scale.get_units(10));
+  //Serial.println("Please put sapmple object on it..."); //提示放上基準物品
 
   dht.begin();//啟動DHT
   pinMode(SWITCH_PIN, INPUT);
-  
-}
-
-void receiveEvent() {
-  k = Wire.read();
-}
-
-void send_data(){
-  Wire.write(current_data[k]);
-
-  Serial.println("------send------");
-  Serial.println(k);
-  Serial.println(current_data[k]);
-
-  Serial.println("-----current----");
-  for(int i=0;i<6;i++) Serial.println(current_data[i]);
-  Serial.println("------end-------");
-
-  if(k==4 || k==5) {
-    current_data[4] = 0;
-    current_data[5] = 0;
-  }
-
-
   
 }
 
@@ -84,45 +44,27 @@ void dht_update(){
   Serial.print("攝氏溫度: ");
   Serial.print(t);
   Serial.println("*C\t");
-
-  current_data[0] = int(h);
-  current_data[1] = 100*h-100*int(h);
-  current_data[2] = int(t);
-  current_data[3] = 100*t-100*int(t);
-  send_data();
 }
 
 void weight_update(){
-  float current_weight = scale.get_units(10);
-  Serial.println("----weight------");
-  Serial.println(current_weight);
-  Serial.println("----------------");
-
-  current_data[4] = int(current_weight);
-  current_data[5] = 100*current_weight - 100*int(current_weight);
-  /*float weight_10 = 0;  
-    for(int i=0;i<10;i++){
-    float w = scale.Get_Weight();
-    Serial.println(w);
-    weight_10 += w;
+  //float current_weight = scale.get_units(10);  // 取得10次數值的平均
+  //float scale_factor=(current_weight/sample_weight);
+  float weight_10 = 0;  
+  //float current_weight = scale.Get_Weight();
+  for(int i=0;i<10;i++){
+    weight_10 += scale.Get_Weight();
     delay(100);
   }
   weight_10 /= 10.0;
   Serial.print("Scale number:  ");
   Serial.println(weight_10*(-1));
-
-  current_data[4] = int(abs(weight_10));
-  current_data[5] = abs(int(abs(weight_10))*100 - abs(weight_10)*100);*/
-
-  
-
-  send_data();
+  //Serial.println(scale_factor,0);  // 顯示比例參數，記起來，以便用在正式的程式中
 }
 
 void loop() {
-  //int switchStatus = Serial.read();
-  int switchStatus = digitalRead(SWITCH_PIN);
-  Serial.println(switchStatus);
+  //int switchStatus = digitalRead(SWITCH_PIN);
+  int switchStatus = Serial.read();
+  //Serial.println(switchStatus);
   bool refresh = 1;
   /*char mode;
   if(Serial.available()>0) mode = Serial.read();
@@ -134,11 +76,11 @@ void loop() {
     dht_update();
     delay(1000);
   }
-  if(switchStatus==0) //opening
+  if(switchStatus=='0') //opening
   {
     isOpen = 1;
   }
-  if(switchStatus==1 && isOpen==1) //closing
+  if(switchStatus=='1' && isOpen==1) //closing
   {
     isOpen = 0;
     weight_update();
