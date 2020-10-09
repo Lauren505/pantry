391cf9745888c5402381aaeff42569a590829b51
zhuangggg <64473970+zhuangggg@users.noreply.github.com>
2020-10-09 22:13:43 +0800

    Add files via upload

--- connection_i2c.py
+++ connection_i2c.py
@@ -1,32 +1,37 @@
 # key input
-# 0: humidity, temperature
-# 1: weight
+# 0,1: humidity
+# 2,3: temperature
+# 4: sign of weight_diff (-:1,+:2)
+# 5,6: weight
+# 7: closing door
 
 from time import sleep
 from smbus import SMBus
 
 addr = 0x04
 bus = SMBus(1)
-msg = []
+msg = [0,0,0,0,0,0]
 current_h = 0
 current_t = 0
 weight = 0
 
 while True:
-    for i in range(0,6):
-		bus.write_byte(addr, i) 
+	for i in range(0,5):
+		bus.write_byte(addr, i)
 		msg[i] = bus.read_byte(addr)
 		time.sleep(3)
-
 	print('msg: {}'.format(msg))
-	
 	current_h = msg[0] + 0.01 * msg[1]
 	current_t = msg[2] + 0.01 * msg[3]
-	weight = msg[4] + 0.01 * msg[5]
+	weight = msg[5] + 0.01 * msg[6]
 	
 	print('h: {}'.format(current_h))
 	print('t: {}'.format(current_t))
 	print('w: {}'.format(weight))
+
+
+
+
 
 
 
