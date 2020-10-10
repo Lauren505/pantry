d51399d8bc229e5c5c92e80c359a89960b89de87
zhuangggg <64473970+zhuangggg@users.noreply.github.com>
2020-10-10 18:27:17 +0800

    Add files via upload

---
+++ static/add_recipe.js
@@ -0,0 +1,11 @@
+const finish = document.getElementById('finish');
+const back = document.getElementsByClassName('back')[0];
+
+back.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/';
+});
+
+finish.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/';
+});
+
--- static/recipe.js
+++ static/recipe.js
@@ -1,4 +1,51 @@
-const recipe = 'http://127.0.0.1:5000/api?action=recipe';
+const showrecipe = 'http://127.0.0.1:5000/api?action=showrecipe';
+const checkrecipe = 'http://127.0.0.1:5000/api?action=checkrecipe';
 
-/*const part = document.getElementById('part')
-const reci = document.getElementById('reci')*/
+const allre = document.getElementById('allre')
+const avail = document.getElementById('avail')
+const allredetail = document.getElementById('allredetail')
+const availdetail = document.getElementById('availdetail')
+const back = document.getElementsByClassName('back')[0];
+const add = document.getElementsByClassName('add')[0];
+
+back.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/';
+});
+
+add.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/addre';
+});
+
+
+function showre(url){
+    fetch(url)
+    .then((response) => {
+    return response.json();
+    }).then((jsontext) =>{
+        console.log(jsontext);
+        allredetail.innerHTML = jsontext;
+    }).catch((err) => {
+    console.log('error:', err);
+    });
+}
+
+function checkre(url){
+    fetch(url)
+    .then((response) => {
+    return response.json();
+    }).then((jsontext) =>{
+        console.log(jsontext);
+        availdetail.innerHTML = jsontext['inv'];
+    }).catch((err) => {
+    console.log('error:', err);
+    });
+}
+
+allre.onclick = function () {
+    showre(showrecipe);
+}
+
+avail.onclick = function () {
+    checkre(checkrecipe);
+}
+
