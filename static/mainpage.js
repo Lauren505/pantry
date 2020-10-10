27e3eeaee22480fdcadb48543c0386848ff21adc
zhuangggg <64473970+zhuangggg@users.noreply.github.com>
2020-10-10 18:08:19 +0800

    Add files via upload

--- static/mainpage.js
+++ static/mainpage.js
@@ -1,12 +1,26 @@
-const mainpage = 'http://127.0.0.1:5000/api?action=update';
-const invpart = 'http://127.0.0.1:5000/api?action=invpart';
+const mainpage = 'http://127.0.0.1:5000/api?action=refresh';
+const updweight = 'http://127.0.0.1:5000/api?action=update';
 
 const temp = document.getElementById('temp')
 const humi = document.getElementById('humi')
 const warn = document.getElementById('warn')
 const refr = document.getElementById('refr')
+const updaw = document.getElementById('updaw')
+const inventory = document.getElementsByClassName('box')[0];
+const recipe = document.getElementsByClassName('box')[1];
+const add = document.getElementsByClassName('box')[2];
 
+inventory.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/inventory';
+});
 
+recipe.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/recipe';
+});
+
+add.addEventListener('click', event=>{
+    window.location.href = 'http://127.0.0.1:5000/add';
+});
 
 function refresh(url){
     fetch(url)
@@ -28,4 +42,19 @@ refr.onclick = function () {
     refresh(mainpage);
 }
 
+function update(url){
+    fetch(url)
+    .then((response) => {
+    return response.json();
+    }).then((jsontext) =>{
+        console.log(jsontext);
+        temp.innerHTML = jsontext['temp'];
+    }).catch((err) => {
+        console.log('refresh error:', err);
+    });
+}
 
+setInterval(refresh(mainpage), 1000);
+refr.onclick = function () {
+    refresh(mainpage);
+}
